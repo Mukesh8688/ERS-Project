158b269760357e68b763d5817c3e4d277c3ae978
MukeshChaudhary <mukesh.chaudhary@revature.net>
2020-12-22 20:31:58 -0600

    edit database connection

--- src/main/java/com/revature/ers/dao/dbutil/ERSDbUtilProperties.java
+++ src/main/java/com/revature/ers/dao/dbutil/ERSDbUtilProperties.java
@@ -4,8 +4,8 @@ public class ERSDbUtilProperties {
 	
 	
 	 public static final String DRIVER = "org.postgresql.Driver";
-	 public static final  String URL = "jdbc:postgresql://localhost:5432/ERS";
+	 public static final  String URL = "jdbc:postgresql://javafs201130.cxs16voijxgr.us-east-1.rds.amazonaws.com:5432/public";
      public static final String USERNAME = "postgres";
-     public static final String PASSWORD = "Revature";
+     public static final String PASSWORD = "password";
 
 }
