493f62a84c7b6e3a2dd24a6168034dff172dfba7
MukeshChaudhary <mukesh.chaudhary@revature.net>
2020-12-22 20:49:24 -0600

    update

--- src/main/java/com/revature/ers/dao/dbutil/ERSDbUtilProperties.java
+++ src/main/java/com/revature/ers/dao/dbutil/ERSDbUtilProperties.java
@@ -7,5 +7,7 @@ public class ERSDbUtilProperties {
 	 public static final  String URL = "jdbc:postgresql://javafs201130.cxs16voijxgr.us-east-1.rds.amazonaws.com:5432/postgres";
      public static final String USERNAME = "postgres";
      public static final String PASSWORD = "password";
+     
+     //sys
 
 }
