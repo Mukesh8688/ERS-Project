1c58d1422387c59f27a2af112034aed681f0eba3
MukeshChaudhary <mukesh.chaudhary@revature.net>
2020-12-22 20:40:32 -0600

    update database name

--- src/main/java/com/revature/ers/dao/dbutil/ERSDbUtilProperties.java
+++ src/main/java/com/revature/ers/dao/dbutil/ERSDbUtilProperties.java
@@ -4,7 +4,7 @@ public class ERSDbUtilProperties {
 	
 	
 	 public static final String DRIVER = "org.postgresql.Driver";
-	 public static final  String URL = "jdbc:postgresql://javafs201130.cxs16voijxgr.us-east-1.rds.amazonaws.com:5432/public";
+	 public static final  String URL = "jdbc:postgresql://javafs201130.cxs16voijxgr.us-east-1.rds.amazonaws.com:5432/postgres";
      public static final String USERNAME = "postgres";
      public static final String PASSWORD = "password";
 
